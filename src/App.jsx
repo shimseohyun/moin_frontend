8e715de47fd47c3e14ae542e524849731e2df1c7
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2023-07-31 16:50:40 +0900

    :sparkles: Feat: router v6으로 바꿈
    router v6버전으로 수정했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './style/globalStyle'
 import { theme } from "./style/theme.js";
+import { Layout } from './Router';
+
 
 function App() {
   return (
     <>
         <ThemeProvider theme={theme}>
           <GlobalStyle />
+          <Layout/>
         </ThemeProvider>
     </>
   )
